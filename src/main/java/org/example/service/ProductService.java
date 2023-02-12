a2012f7f66c1c1048d36dd6007f7477f85ae6a67
vuongNguyenVg <vuongnguyen332003@gmail.com>
2023-02-12 09:00:24 +0700

    demo_thymeleaf

---
+++ src/main/java/org/example/service/ProductService.java
@@ -0,0 +1,16 @@
+package org.example.service;
+
+import org.example.model.Product;
+
+import java.util.List;
+
+public interface ProductService {
+   List <Product> findAll();
+
+   void save(Product product);
+
+   Product findById(int id);
+
+   void update (int id, Product product);
+   void remove(int id);
+}
